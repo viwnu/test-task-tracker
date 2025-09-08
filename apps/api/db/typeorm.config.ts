1d9a871beab3acede449b4dfdfe03ae5a1e52e83
viwnu <xenmysafitiplace@gmail.com>
2025-09-08 10:46:34 +0300

    refactored yarn to npm

--- apps/api/db/typeorm.config.ts
+++ apps/api/db/typeorm.config.ts
@@ -3,34 +3,19 @@ import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { ENTITIES } from './entities';
 
 export const TypeOrmConfigService = (): TypeOrmModuleAsyncOptions => ({
-  useFactory: (configService: ConfigService) => {
-    console.log({
-      type: 'postgres',
-      host: configService.get('PS_HOST'),
-      port: Number(configService.get('PS_PORT')),
-      username: configService.get('PS_USER'),
-      password: configService.get('PS_PASSWORD'),
-      database: configService.get('PS_DB_NAME'),
-      entities: ENTITIES,
-      autoLoadEntities: Boolean(configService.get('SYNC_DB') === 'true') || false,
-      synchronize: Boolean(configService.get('SYNC_DB') === 'true') || false,
-      logger: 'simple-console',
-      logging: Boolean(configService.get('TYPEORM_LOGGING') === 'true') || false,
-    });
-    return {
-      type: 'postgres',
-      host: configService.get('PS_HOST'),
-      port: Number(configService.get('PS_PORT')),
-      username: configService.get('PS_USER'),
-      password: configService.get('PS_PASSWORD'),
-      database: configService.get('PS_DB_NAME'),
-      entities: ENTITIES,
-      autoLoadEntities: Boolean(configService.get('SYNC_DB') === 'true') || false,
-      synchronize: Boolean(configService.get('SYNC_DB') === 'true') || false,
-      logger: 'simple-console',
-      logging: Boolean(configService.get('TYPEORM_LOGGING') === 'true') || false,
-    };
-  },
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('PS_HOST'),
+    port: Number(configService.get('PS_PORT')),
+    username: configService.get('PS_USER'),
+    password: configService.get('PS_PASSWORD'),
+    database: configService.get('PS_DB_NAME'),
+    entities: ENTITIES,
+    autoLoadEntities: Boolean(configService.get('SYNC_DB') === 'true') || false,
+    synchronize: Boolean(configService.get('SYNC_DB') === 'true') || false,
+    logger: 'simple-console',
+    logging: Boolean(configService.get('TYPEORM_LOGGING') === 'true') || false,
+  }),
   inject: [ConfigService],
   imports: [ConfigModule],
 });
